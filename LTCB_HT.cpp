7d7492efe66ab64feafed9c4d6a893110e7119b6
TamNhien <canhthien91@gmail.com>
2024-03-18 08:26:38 +0700

    Add project files.

---
+++ LTCB_HT.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    const double pi = 3.14159;
+    double r;
+    cout << "Nhap ban kinh hinh tron: ";
+    cin >> r;
+
+    double chuvi = 2 * pi * r;
+    double dientich = pi * pow(r, 2);
+
+    cout << "Chu vi cua hinh tron la: " << chuvi << endl;
+    cout << "Dien tich cua hinh tron la: " << dientich << endl;
+
+    return 0;
+}
